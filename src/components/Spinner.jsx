dda89eae9131aa501cd0c84de76048a674e40c80
Jon Arryn <87862871+JonArryn@users.noreply.github.com>
2022-03-06 12:25:25 -0800

    Add Spinner file via upload to correct filename misspelling

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// why am I having a problem
+
+function Spinner() {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
